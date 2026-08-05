5f28c317fa091a64a94b522417ecfb842146f346
Aakash0057 <Batch1@VITUNIVERSITY.LOCAL>
2026-08-05 16:47:08 +0530

    first commit

---
+++ strlen.py
@@ -0,0 +1,12 @@
+def count_vowels(s):
+    c=0
+    for i in s:
+        if(i == 'a'or'e'or'i'or'o'or'u'):
+            c+=1
+    return c
+
+
+if __name__  == "__main__":
+    str = "programming"  
+    res=count_vowels(str)
+    print(res)
