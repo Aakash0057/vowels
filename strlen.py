15dcc6627758c7c984a0804cbac9ce4ea7d1c321
Aakash0057 <Batch1@VITUNIVERSITY.LOCAL>
2026-08-05 16:53:37 +0530

    added count_consonents

--- strlen.py
+++ strlen.py
@@ -4,9 +4,16 @@ def count_vowels(s):
         if(i == 'a'or'e'or'i'or'o'or'u'):
             c+=1
     return c
-
+def count_consonants(s):
+    c=0
+    for i in s:
+        if(i!='a'or'e'or'i'or'o'or'u'):
+            c+=1
+    return c
 
 if __name__  == "__main__":
     str = "programming"  
     res=count_vowels(str)
     print(res)
+    res1=count_consonants(str)
+    print(res1)
